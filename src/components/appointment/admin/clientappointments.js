17962f487c071b22ce7a0f07d36c0a4be2b5e913
thishonestabe <marquez.abraham1991@gmail.com>
2020-11-23 01:31:59 +0100

    clientAppointments component added

---
+++ src/components/appointment/admin/clientappointments.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const clientAppointments = (props) => {
+    const style = {
+
+    };
+    return (
+        <div className={"clientAppointments"} style={style}>
+
+        </div>
+
+    )
+}
+
+export default clientAppointments
