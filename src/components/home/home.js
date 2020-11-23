32f5caaf916a2907ae1f4868a1ab697fd402259e
thishonestabe <marquez.abraham1991@gmail.com>
2020-11-23 01:23:13 +0100

    home component added

---
+++ src/components/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const home = (props) => {
+    const style = {
+
+    };
+    return (
+        <div className={"Home"} style={style}>
+
+        </div>
+
+    )
+}
+
+export default home
