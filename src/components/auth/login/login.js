4d6e7c6cf935de3a50671fb91edc765305c1eaea
thishonestabe <marquez.abraham1991@gmail.com>
2020-11-23 01:27:10 +0100

    login component added

---
+++ src/components/auth/login/login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const login = (props) => {
+    const style = {
+
+    };
+    return (
+        <div className={"Login"} style={style}>
+
+        </div>
+
+    )
+}
+
+export default login
