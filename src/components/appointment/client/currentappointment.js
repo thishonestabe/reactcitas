1a19059a9850998bdb20384f79bfc655a1ca8b4c
thishonestabe <marquez.abraham1991@gmail.com>
2020-11-23 01:34:42 +0100

    currentAppointment component added

---
+++ src/components/appointment/client/currentappointment.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const currentAppointment = (props) => {
+    const style = {
+
+    };
+    return (
+        <div className={"currentAppointment"} style={style}>
+
+        </div>
+
+    )
+}
+
+export default currentAppointment
