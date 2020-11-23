046c1a9a13bd370d6d7310923ca9ea4b82095973
thishonestabe <marquez.abraham1991@gmail.com>
2020-11-23 02:37:39 +0100

    Login component presentation completed

--- src/components/auth/login/login.js
+++ src/components/auth/login/login.js
@@ -1,11 +1,85 @@
 import React from 'react';
+import { Card, Row, Col, Form, Input, Button, Checkbox } from 'antd';
+
 
 const login = (props) => {
     const style = {
 
     };
+    const layout = {
+        labelCol: {
+            span: 4,
+        },
+        wrapperCol: {
+            span: 20,
+        },
+    };
+    const tailLayout = {
+        wrapperCol: {
+            offset: 6,
+            span: 12,
+        },
+    };
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
     return (
         <div className={"Login"} style={style}>
+            <Row>
+                <Col span={12} offset={6}>
+                    <Card title="Login" style={{margin: 'auto'}}>
+                        <Form
+                            {...layout}
+                            name="basic"
+                            initialValues={{
+                                remember: true,
+                            }}
+                            onFinish={onFinish}
+                            onFinishFailed={onFinishFailed}
+                        >
+                            <Form.Item
+                                label="Username"
+                                name="username"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your username!',
+                                    },
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item
+                                label="Password"
+                                name="password"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your password!',
+                                    },
+                                ]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+
+                            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                                <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+
+                            <Form.Item {...tailLayout}>
+                                <Button type="primary" htmlType="submit">
+                                    Submit
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </Card>
+                </Col>
+            </Row>
 
         </div>
 
