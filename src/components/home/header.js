1397a162a3e30103e93971af8059f8182e2b49b6
thishonestabe <marquez.abraham1991@gmail.com>
2020-11-23 03:05:42 +0100

    header component content added

--- src/components/home/header.js
+++ src/components/home/header.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import { PageHeader } from 'antd';
 const header = (props) => {
     const style = {
 
     };
     return (
         <div className={"Header"} style={style}>
-
+            <PageHeader
+                className="site-page-header"
+                onBack={() => null}
+                title="Title"
+                subTitle="This is a subtitle"
+            />
         </div>
 
     )
