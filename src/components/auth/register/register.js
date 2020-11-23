37ef68d684db79c4c25f8e7ca93da145155a13cf
thishonestabe <marquez.abraham1991@gmail.com>
2020-11-23 01:25:33 +0100

    register component added

---
+++ src/components/auth/register/register.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const register = (props) => {
+    const style = {
+
+    };
+    return (
+        <div className={"Register"} style={style}>
+
+        </div>
+
+    )
+}
+
+export default register
